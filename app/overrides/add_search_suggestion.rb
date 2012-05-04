a91963f4df042838f86a2f38e0f1f935ac507068
Michael Bianco <iloveitaly@gmail.com>
2012-05-04 15:02:01 -0400

    Disabling suggestion deface. Sunspot does not support spell checking yet

--- app/overrides/add_search_suggestion.rb
+++ app/overrides/add_search_suggestion.rb
@@ -1,5 +1,8 @@
-Deface::Override.new(:virtual_path => "spree/products/index",
-                      :name => "show_search_partials_suggestion",
-                      :insert_top => "[data-hook='search_results']",
-                      :partial => "spree/products/suggestion",
-                      :disabled => false)
+# unfortunately it doesn't look like sunspot has spell check support yet
+#   https://github.com/sunspot/sunspot/pull/43
+
+# Deface::Override.new(:virtual_path => "spree/products/index",
+#                       :name => "show_search_partials_suggestion",
+#                       :insert_top => "[data-hook='search_results']",
+#                       :partial => "spree/products/suggestion",
+#                       :disabled => false)
