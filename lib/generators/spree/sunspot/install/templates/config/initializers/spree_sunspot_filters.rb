cedb99511285481b89ffef26ad4b82b4915f1117
John Dilts <iam@hybridindie.com>
2013-01-23 19:01:37 -0500

    document the filters a little

--- lib/generators/spree/sunspot/install/templates/config/initializers/spree_sunspot_filters.rb
+++ lib/generators/spree/sunspot/install/templates/config/initializers/spree_sunspot_filters.rb
@@ -1,22 +1,28 @@
 Spree::Sunspot::Setup.filters do |filters|
-  # Maximum value would be ignored if the range maximum is set to
-  # Spree::Sunspot::Setup::IGNORE_MAX
-  filters.add do |f|
-    f.search_param = 'price'
-    f.display_name = 'Price'
-    f.values { [0..10, 10..20, 20..30, 30..Spree::Sunspot::Setup::IGNORE_MAX] }
-  end
 
-  # Additional examples
+  # Ranges can be Dates, Ints, or Floats
+  # filters.add do |f|
+  #   f.search_param = 'price'
+  #   # Can be :any (||) or :all (&&)
+  #   f.search_condition = :any
+  #   f.display_name = 'Price'
+  #   f.values { [0..10, 10..20, 20..30] }
+  # end
+
+  # Pre load at runtime or specify items for restrictions
   # filters.add do |f|
   #   f.search_param = 'category_name'
+  #   f.search_condition = :any
   #   f.display_name = 'Category'
   #   f.values { Spree::Taxon.find_by_permalink('categories').children.map(&:name) }
   # end
-  #
+
+  # Empty arrays eager load facets specified here
   # filters.add do |f|
   #   f.search_param = 'brand_name'
+  #   f.search_condition = :any
   #   f.display_name = 'Brand'
-  #   f.values { Spree::Taxon.find_by_permalink('brands').children.map(&:name) }
+  #   f.values { [] }
   # end
+
 end
